18dadd1589e28252a870845397b9b35f98a9b5ac
vineet416 <vineetpatel468@gmail.com>
2025-11-18 10:41:31 +0530

    updated API endpoints

--- main.py
+++ main.py
@@ -26,36 +26,6 @@ class apidata(BaseModel):
     course: str
 
 
-## API Endpoints for Insert
-@app.post("/api/insert")
-async def api_data_insert(data:apidata):
-    result = await api_data.insert_one(data.dict())
-    if result.inserted_id:
-        return {"message": "Data inserted successfully", "id": str(result.inserted_id)}
-    else:
-        raise HTTPException(status_code=500, detail="Data insertion failed")
-
-
-## API Endpoints for Update
-@app.post("/api/update")
-async def api_data_update(name: str, data:apidata):
-    result = await api_data.update_one({"name": name}, {"$set": data.dict()})
-    if result.modified_count == 1:
-        return {"message": "Data updated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Data not found")
-
-
-## API Endpoints for Delete
-@app.post("/api/delete")
-async def api_data_delete(name: str):
-    result = await api_data.delete_one({"name": name})
-    if result.deleted_count == 1:
-        return {"message": "Data deleted successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Data not found")
-
-
 ## API Helper function to format data
 def api_helper(doc):
     doc["id"] = str(doc["_id"])
@@ -71,3 +41,43 @@ async def get_api_data():
     async for document in cursor:
         items.append(api_helper(document))
     return items
+
+
+## API Endpoints for Insert 
+@app.post("/api/insert")
+async def api_data_insert(data:apidata):
+    result = await api_data.insert_one(data.dict())
+    if result.inserted_id:
+        return {"message": "Data inserted successfully", "id": str(result.inserted_id)}
+    else:
+        raise HTTPException(status_code=500, detail="Data insertion failed")
+
+
+## API Endpoints for full update
+@app.put("/api/fullupdate")
+async def api_data_full_update(id: str, data:apidata):
+    result = await api_data.update_one({"_id": ObjectId(id)}, {"$set": data.dict()})
+    if result.modified_count == 1:
+        return {"message": "Data fully updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Data not found")
+
+
+## API Endpoints for partial update
+@app.patch("/api/partialupdate")
+async def api_data_partial_update(id: str, data: dict):
+    result = await api_data.update_one({"_id": ObjectId(id)}, {"$set": data})
+    if result.modified_count == 1:
+        return {"message": "Data partially updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Data not found")
+
+
+## API Endpoints for Delete
+@app.delete("/api/delete")
+async def api_data_delete(id: str):
+    result = await api_data.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count == 1:
+        return {"message": "Data deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Data not found")
